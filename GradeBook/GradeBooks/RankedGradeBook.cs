53b778745051abd00b54a018c05d258c2595ca57
atarnawski <54600159+atarnawski@users.noreply.github.com>
2019-08-27 22:50:37 +0200

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,11 +13,30 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade(double averageGrade)
         {
-            if(Students.Count < 5)
+            var counter = 0;
+            var listPosition = 0;
+            if (Students.Count < 5)
             {
                 throw new InvalidOperationException();
             }
-
+            else
+            {
+                var averageList = new List<Double>();
+                foreach (var student in Students)
+                {
+                    averageList.Add(student.AverageGrade);
+                }
+                averageList.Sort();
+                foreach(var averageGradeList in averageList)
+                {
+                    if(averageGrade < averageGradeList) { listPosition = counter; }
+                    counter++;
+                }
+                if(listPosition < 0.2 * Students.Count) { return 'A'; }
+                if (listPosition < 0.4 * Students.Count) { return 'B'; }
+                if (listPosition < 0.6 * Students.Count) { return 'C'; }
+                if (listPosition < 0.8 * Students.Count) { return 'D'; }
+            }
             return 'F';
         }
     }
